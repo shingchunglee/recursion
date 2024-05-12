d5505e9e8af2075c460ccbd3d3a5424988b5fb5c
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-05-13 02:32:49 +1000

    add team asian bois

---
+++ Assets/IntroManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(LoadNext());
+    }
+
+    IEnumerator LoadNext()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
